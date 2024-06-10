449dc9f33c1df4506762dbc75d2edaff909eefab
Foca Bogdan <81586357+Zeyt8@users.noreply.github.com>
2024-06-10 17:47:50 +0300

    Redone main menu ui

--- Assets/_Pisicute/Scripts/MainMenuManager.cs
+++ Assets/_Pisicute/Scripts/MainMenuManager.cs
@@ -1,42 +1,11 @@
 using System;
-using TMPro;
-using Unity.Netcode;
-using Unity.Services.Lobbies.Models;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
-public class MainMenuManager : NetworkSingleton<MainMenuManager>
+public class MainMenuManager : MonoBehaviour
 {
-    public string levelScene;
     [Header("UI Elements")]
     [SerializeField] private LoadingPanel loadingPanel;
-    [SerializeField] private TMP_InputField createLobbyNameInput;
-    [SerializeField] private TMP_InputField joinPrivateLobbyCodeInput;
-    [SerializeField] private Toggle privateLobbyToggle;
-    [SerializeField] private PlayerLobby playerLobby;
-
-    private Lobby currentSelectedLobby;
-
-    private void Start()
-    {
-        ConnectToServer();
-    }
-
-    public void SetPlayerName(string playerName)
-    {
-        NetworkHandler.PlayerName = playerName;
-    }
-
-    public void StartGame()
-    {
-        if (GameManager.SelectedMap.Value.IsEmpty)
-        {
-            loadingPanel.ShowLoad(LoadingType.Error, "Select a map");
-            return;
-        }
-        NetworkManager.Singleton.SceneManager.LoadScene(levelScene, LoadSceneMode.Single);
-    }
 
     public async void ConnectToServer()
     {
@@ -52,103 +21,14 @@ public class MainMenuManager : NetworkSingleton<MainMenuManager>
         }
     }
 
-    public async void JoinLobby()
-    {
-        loadingPanel.ShowLoad(LoadingType.JoiningRoom);
-        try
-        {
-            NetworkManager.Singleton.Shutdown();
-            await LobbyHandler.JoinLobby(currentSelectedLobby.Id, JoinLobbyMode.Id);
-            loadingPanel.gameObject.SetActive(false);
-
-            NetworkManager.Singleton.StartClient();
-            playerLobby.gameObject.SetActive(true);
-        }
-        catch (Exception e)
-        {
-            if (currentSelectedLobby == null)
-                loadingPanel.ShowLoad(LoadingType.Error, "No lobby selected");
-            else
-                loadingPanel.ShowLoad(LoadingType.Error, e.Message);
-        }
-    }
-
-    public async void JoinPrivateLobby()
-    {
-        loadingPanel.ShowLoad(LoadingType.JoiningRoom);
-        try
-        {
-            NetworkManager.Singleton.Shutdown();
-            await LobbyHandler.JoinLobby(joinPrivateLobbyCodeInput.text, JoinLobbyMode.Code);
-            loadingPanel.gameObject.SetActive(false);
-
-            NetworkManager.Singleton.StartClient();
-            playerLobby.gameObject.SetActive(true);
-        }
-        catch (Exception e)
-        {
-            if (string.IsNullOrEmpty(joinPrivateLobbyCodeInput.text))
-                loadingPanel.ShowLoad(LoadingType.Error, "Please enter a lobby code");
-            else
-                loadingPanel.ShowLoad(LoadingType.Error, e.Message);
-        }
-    }
-
-    public async void CreateLobby()
-    {
-        loadingPanel.ShowLoad(LoadingType.CreatingRoom);
-        try
-        {
-            NetworkManager.Singleton.Shutdown();
-            await LobbyHandler.CreateLobby(createLobbyNameInput.text, privateLobbyToggle.isOn);
-            loadingPanel.gameObject.SetActive(false);
-            NetworkManager.Singleton.StartHost();
-            playerLobby.gameObject.SetActive(true);
-        }
-        catch (Exception e)
-        {
-            if (string.IsNullOrEmpty(createLobbyNameInput.text))
-                loadingPanel.ShowLoad(LoadingType.Error, "Please enter a lobby name");
-            else
-                loadingPanel.ShowLoad(LoadingType.Error, e.Message);
-        }
-    }
-
-    public void SetCurrentSelectedLobby(Lobby lobby)
-    {
-        currentSelectedLobby = lobby;
-    }
-
-    public async void LeaveLobby()
-    {
-        try
-        {
-            NetworkManager.Singleton.Shutdown();
-            if (LobbyHandler.IsLobbyHost)
-            {
-                await LobbyHandler.DeleteLobby();
-            }
-            else
-            {
-                await LobbyHandler.LeaveLobby();
-            }
-            playerLobby.gameObject.SetActive(false);
-            RefreshLobbyList();
-        }
-        catch (Exception e)
-        {
-            loadingPanel.ShowLoad(LoadingType.Error, e.Message);
-        }
-    }
-
-    public void RefreshLobbyList()
-    {
-        LobbyCallbacks.RefreshLobbyList();
-    }
-
     public void DisconnectFromServer()
     {
         NetworkHandler.DisconnectFromServer();
+    }
+
+    public void ChangeScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
     }
 
     public void QuitGame()
